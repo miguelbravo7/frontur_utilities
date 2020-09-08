bd21a331018a3d7a71516e6b60a4345ccd180705
alu0101031538 <alu0101031538@ull.edu.es>
2020-09-08 20:27:21 +0100

    Minor version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="Trabajo de fin de grado",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.1.4",
+    version="0.2.0",
     packages=find_namespace_packages(),
     include_package_data=True,
 
